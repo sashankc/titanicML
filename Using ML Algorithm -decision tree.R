acd32d14b365ae86ba62c422775358b157cfbdbc
sashankc <sashankt@gmail.com>
2016-07-03 10:39:49 -0700

    Decision Tree ML Algorithm

---
+++ Using ML Algorithm -decision tree.R
@@ -0,0 +1,12 @@
+# Using the Decision Tree ML algorithm
+
+# import the library
+library(rpart)
+
+# call rpart method
+fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked,
+             data=train,
+             method="class")
+
+# plot the output
+plot(fit)
