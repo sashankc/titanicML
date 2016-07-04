c1b7d6706f704dac86508c2df868cc798c515fa8
sashankc <sashankt@gmail.com>
2016-07-04 14:39:25 -0700

    Prediction after doing feature engineering

---
+++ Prediction3-post feature engineering.R
@@ -0,0 +1,7 @@
+#Predict that everyone died on the boat
+train <- combi[1:891,]
+test <- combi[892:1309,]
+
+fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked + Title + FamilySize + FamilyID,
+             data=train, 
+             method="class")
